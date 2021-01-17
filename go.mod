dc91046cb6d5644d3cb6130c86cb4c1d095e78c0
Bryan Lopez <lopez95.bryan@gmail.com>
2021-01-17 15:28:54 -0500

    Add middleware and versioning package

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/BryanL95/nrfiber
+
+go 1.15
+
+require (
+	github.com/gofiber/fiber/v2 v2.3.3
+	github.com/newrelic/go-agent/v3 v3.10.0
+)
--- nrfiber.go
+++ nrfiber.go
@@ -1,5 +1,41 @@
 package nrfiber
 
-func Middleware() {
+import (
+	"net/http"
+	"net/url"
 
+	"github.com/gofiber/fiber/v2"
+	"github.com/newrelic/go-agent/v3/newrelic"
+)
+
+func Middleware(app *newrelic.Application) fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		if app != nil {
+			txn := app.StartTransaction(c.Method() + " " + c.Path())
+			defer func() {
+				err := recover()
+				if err != nil {
+					switch err := err.(type) {
+					case error:
+						txn.NoticeError(err)
+					}
+				} else {
+					txn.SetWebRequest(newrelic.WebRequest{
+						Header: http.Header{
+							"Response-Headers": []string{
+								c.Get("Date"),
+							},
+						},
+						URL:       &url.URL{Path: c.Path()},
+						Method:    c.Method(),
+						Transport: newrelic.TransportHTTP,
+					})
+				}
+				txn.End()
+			}()
+			c.Context().SetUserValue("__newrelic_transaction__", txn)
+		}
+		c.Next()
+		return nil
+	}
 }
