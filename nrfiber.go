27c7982ce70ecfa282735635d03ce8c514c1d7c0
Bryan Lopez <lopez95.bryan@gmail.com>
2021-01-18 12:29:16 -0500

    v1.1.0 - Add error message and logs by http status code if the code it's different 200

--- nrfiber.go
+++ nrfiber.go
@@ -3,6 +3,7 @@ package nrfiber
 import (
 	"net/http"
 	"net/url"
+	"strconv"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/newrelic/go-agent/v3/newrelic"
@@ -20,16 +21,26 @@ func Middleware(app *newrelic.Application) fiber.Handler {
 						txn.NoticeError(err)
 					}
 				} else {
-					txn.SetWebRequest(newrelic.WebRequest{
-						Header: http.Header{
-							"Response-Headers": []string{
-								c.Get("Date"),
+					if c.Response().StatusCode() != 200 {
+						txn.NoticeError(newrelic.Error{
+							Message: http.StatusText(c.Response().StatusCode()),
+							Class:   strconv.Itoa(c.Response().StatusCode()),
+							Attributes: map[string]interface{}{
+								"Host":      c.Hostname(),
+								"Url":       c.Path(),
+								"UserAgent": c.Get("User-Agent"),
 							},
-						},
-						URL:       &url.URL{Path: c.Path()},
-						Method:    c.Method(),
-						Transport: newrelic.TransportHTTP,
-					})
+						})
+					} else {
+						txn.SetWebRequest(newrelic.WebRequest{
+							Header: http.Header{
+								"statusCode": []string{strconv.Itoa(c.Response().StatusCode())},
+							},
+							URL:       &url.URL{Path: c.Path()},
+							Method:    c.Method(),
+							Transport: newrelic.TransportHTTP,
+						})
+					}
 				}
 				txn.End()
 			}()
